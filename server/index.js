ca7f55cba89507acd920d4bcfa7b2b9913a8034d
yuki <s1932058jl@s.chibakoudai.jp>
2021-09-20 16:04:20 +0900

    establish the connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
 
 
-const app = express();
+const connectOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+mongoose.connect(
+    process.env.CONNECTION_URL,
+    connectOptions,
+    () => {console.log("Success!")}
+);
+
 
 const port = process.env.PORT || 5000;
 
+const app = express();
+
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
