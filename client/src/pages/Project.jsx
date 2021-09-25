a9b6209ddd52b7c9e38dd1d788a8fe6d258c5e25
yuki <s1932058jl@s.chibakoudai.jp>
2021-09-25 14:56:03 +0900

    changed the way of creating project

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,7 +1,12 @@
+import { useHistory } from 'react-router-dom'
+
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
 
 import AppBar from '../components/AppBar';
 
@@ -12,22 +17,36 @@ import { ReactComponent as EmptyLogo } from '../assets/undraw_empty_street_sfxm.
 const Project = () => {
     const projects = useFetch();
 
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push("/projects/new");
+    }
+
     return (
         <>
             <AppBar />
             <Container>
-                {projects ? projects.map(project => (
+                {projects !== [] ? projects.map(project => (
                     <Grid container>
-                        <Grid item sm key={project.id}>
+                        <Grid item sm key={project._id}>
                             <Box>
-                                <Typography variant="body2">{project.name}</Typography>
+                                <Typography variant="body2">{project._id}</Typography>
                             </Box>
                         </Grid>
                     </Grid>
                 )) : (
-                    <EmptyLogo width="60%" height="60%" />
+                    <>
+                        <EmptyLogo width="80%" height="80%" />
+                        <Typography variant="h6">プロジェクトを作成しましょう</Typography>
+                    </>
                 )}
             </Container>
+            <Box sx={{position: "absolute", bottom: 50, right: 50, }}>
+                <IconButton onClick={handleClick} color="primary">
+                    <AddIcon size="large"/>
+                </IconButton>
+            </Box>
         </>
     )
 };
