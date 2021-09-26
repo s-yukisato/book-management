db8436b557f6a7348efee4800897fda50f32e6bc
yuki <s1932058jl@s.chibakoudai.jp>
2021-09-26 16:32:31 +0900

    update validation

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -6,7 +6,6 @@ import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
@@ -16,6 +15,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import UserName from '../components/UserName';
 import Email from '../components/Email';
 import Password from '../components/Password';
 import Copyright from '../components/Copyright';
@@ -24,19 +24,16 @@ const theme = createTheme();
 
 const SignUp = () => {
     const [values, setValues] = useState({
-        firstName: '',
-        lastName: '',
+        name: '',
         email: '',
         password: '',
-    })
-    const handleSubmit = async () => {
-        await axios.post('http://localhost:3001/api/v1/user', values)
-            .then(response => response)
-    };
+    });
 
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-        console.log(values)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { data, error } = await axios.post('http://localhost:3001/api/v1/user/signup', values);
+        console.log(data)
+        console.log(error)
     };
 
     return (
@@ -59,28 +56,8 @@ const SignUp = () => {
                     </Typography>
                     <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="fname"
-                                    name="firstName"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    onChange={handleChange("firstName")}
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="lname"
-                                    onChange={handleChange("lastName")}
-                                />
+                            <Grid item xs={12}>
+                                <UserName values={values} setValues={setValues} />
                             </Grid>
                             <Grid item xs={12}>
                                 <Email values={values} setValues={setValues} />
@@ -100,14 +77,13 @@ const SignUp = () => {
                             fullWidth
                             onClick={handleSubmit}
                             variant="contained"
-                            disabled={values}
                             sx={{ mt: 3, mb: 2 }}
                         >
                             Sign Up
                         </Button>
                         <Grid container justifyContent="flex-end">
                             <Grid item>
-                                <RouterLink to="/login">
+                                <RouterLink to="/signin">
                                     Already have an account? Sign in
                                 </RouterLink>
                             </Grid>
