714df46d09c70e6535ada5bb2e2e9817a9788f3f
yuki <s1932058jl@s.chibakoudai.jp>
2021-09-26 16:38:02 +0900

    add signin page

---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+import Password from '../components/Password';
+import Email from '../components/Email';
+import Copyright from '../components/Copyright';
+
+const theme = createTheme();
+
+export default function SignIn() {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleSubmit = () => { }
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Email values={values} setValues={setValues} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Password values={values} setValues={setValues} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                    label="I want to receive inspiration, marketing promotions and updates via email."
+                                />
+                            </Grid>
+                        </Grid>
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <RouterLink to="/account">
+                                    Forgot password?
+                                </RouterLink>
+                            </Grid>
+                            <Grid item>
+                                <RouterLink to="/signup">
+                                    {"Don't have an account? Sign Up"}
+                                </RouterLink>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
