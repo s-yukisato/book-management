6285d286967f06850b5d603cd96e1796437f0098
yuki <s1932058jl@s.chibakoudai.jp>
2021-09-27 07:31:31 +0900

    add jsonwebtoken

--- server/index.js
+++ server/index.js
@@ -1,41 +1,61 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const expressJwt = require("express-jwt");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 
-const library = require('./routes/library');
-const data = require('./routes/data');
-const project = require('./routes/project');
+const authRoute = require("./routes/auth");
+const data = require("./routes/data");
+const library = require("./routes/library");
+const project = require("./routes/project");
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 const connectOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-mongoose.connect(
-    process.env.CONNECTION_URL,
-    connectOptions,
-    () => {console.log("Success!")}
-);
-
+mongoose.connect(process.env.CONNECTION_URL, connectOptions, () => {
+  console.log("Success!");
+});
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
-app.get('/', (req, res) => {
-    return res.send("Hello!")
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use("/api/v1/user", authRoute);
+app.use("/api/data", data);
+app.use("/api/v1/library", library);
+app.use("/api/v1/document", project);
+
+app.use(cookieParser());
+app.use(
+  expressJwt({
+    secret: process.env.ACCESS_TOKEN_SECRET,
+    algorithms: ["HS256"],
+    getToken: (req) => req.cookies.token,
+  })
+);
+const csrfProtection = csrf({
+  cookie: true,
+});
+app.use(csrfProtection);
+
+app.get("/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
 });
 
-app.use('/api/data', data); 
-app.use('/api/v1/library', library);
-app.use('/api/v1/document', project);
-
 app.listen(port, (req, res) => {
-    console.log(`listening on port ${port}!`);
-})
+  console.log(`listening on port ${port}!`);
+});
