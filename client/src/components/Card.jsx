d6ca5e0bc80f32208c1291a30214a4f35df0dc98
yuki <s1932058jl@s.chibakoudai.jp>
2021-09-30 23:16:18 +0900

    clean dialog component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,6 +6,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 import IconButton from '@mui/material/IconButton';
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
@@ -13,21 +14,48 @@ import AddIcon from '@mui/icons-material/Add';
 
 import LightTooltip from './LightTooltip';
 import Modal from './Modal';
+import Dialog from './Dialog';
+
+import { ReactComponent as AuthenticationLogo } from '../assets/undraw_authentication_fsn5.svg'
 
 
-const MediaCard = ({ book }) => {
+const MediaCard = ({ book, registeredList, user }) => {
     const history = useHistory();
 
-    const [registered, setRegistered] = useState(false);
+    const [registered, setRegistered] = useState(registeredList.includes(book.isbn));
 
     const [open, setOpen] = useState(false);
+    const [dialog, setDialog] = useState(false);
 
     const register = () => {
-        setOpen(true);
+        if (user) {
+            setOpen(true);
+        } else {
+            setDialog(true);
+        }
     }
 
     const unregister = () => {
         history.push("/library");
+    }
+
+    const redirectSignup = () => {
+        history.push('/signup');
+    }
+
+    const redirectSignin = () => {
+        history.push('/signin');
+    }
+
+    const values = {
+        title: "このサービスはログインが必要です",
+        content: <AuthenticationLogo width="80%" height="80%" />,
+        actions: (
+            <>
+                <Button onClick={redirectSignup}>サインアップ</Button>
+                <Button onClick={redirectSignin}>サインイン</Button>
+            </>
+        )
     }
 
     return (
@@ -76,6 +104,7 @@ const MediaCard = ({ book }) => {
                     </LightTooltip>
                 )}
                 <Modal open={open} setOpen={setOpen} setRegistered={setRegistered} book={book} />
+                <Dialog values={values} open={dialog} setOpen={setDialog} />
             </CardActions>
         </Card>
     );
--- client/src/components/Dialog.jsx
+++ client/src/components/Dialog.jsx
@@ -1,31 +1,16 @@
-import { useHistory } from 'react-router-dom';
-
-import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Divider from '@mui/material/Divider';
 
-import { ReactComponent as AuthenticationLogo } from '../assets/undraw_authentication_fsn5.svg'
 
-
-export default function AlertDialogSlide({open, setOpen}) {
-    const history = useHistory();
+const DialogComponent = ({ values, open, setOpen }) => {
 
     const handleClose = () => {
         setOpen(false);
     };
-
-    const handleClickSignIn = () => {
-        setOpen(false);
-        history.push('/signin')
-    }
-
-    const handleClickSignUp = () => {
-        setOpen(false);
-        history.push('/signup')
-    }
+    const { title, content, actions } = values;
 
     return (
         <>
@@ -35,16 +20,14 @@ export default function AlertDialogSlide({open, setOpen}) {
                 onClose={handleClose}
                 aria-describedby="alert-dialog-slide-description"
             >
-                <DialogTitle>このサービスはログインが必要です</DialogTitle>
+                <DialogTitle>{title}</DialogTitle>
                 <Divider />
-                <DialogContent>
-                    <AuthenticationLogo width="80%" height="80%" />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClickSignIn}>サインイン</Button>
-                    <Button onClick={handleClickSignUp}>サインアップ</Button>
-                </DialogActions>
+                <DialogContent>{content}</DialogContent>
+                <DialogActions>{actions}</DialogActions>
             </Dialog>
         </>
     );
 }
+
+export default DialogComponent
+
