8deb1ebbef4304443d5f78c602c898580ac3ad87
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-01 17:59:52 +0900

    Update Support.jsx

--- client/src/pages/Support.jsx
+++ client/src/pages/Support.jsx
@@ -12,8 +12,9 @@ import Email from '../components/Email';
 import Fab from '@mui/material/Fab';
 import SendIcon from '@mui/icons-material/Send';
 
-import AppBar from '../components/AppBar';
+import AppBar from '../components/AppBar2';
 import Footer from '../components/Footer';
+import Navi from '../components/Navi';
 
 import { ReactComponent as QuestionLogo } from '../assets/undraw_Questions_re_1fy7.svg'
 
@@ -42,49 +43,74 @@ const Support = () => {
     return (
         <>
             <AppBar />
-            <Grid container sx={{ m: 'auto', width: "90%" }}>
-                <Grid item sm={12} sx={{ textAlign: 'center', m: 3 }}>
-                    <Typography variant="h3" color="inherit">
-                        お困りですか?
-                    </Typography>
+            <Grid container>
+                <Grid
+                    container
+                    width="250px"
+                    sx={{
+                        display: { xs: 'none', md: 'block' },
+                        borderRight: '2px solid #f5f0ec',
+                        bgcolor: "#FCFAF9"
+                    }}
+                >
+                    <Navi />
                 </Grid>
-                <Grid container spacing={3} sx={{ flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-evenly', p: 2 }}>
-                    <Grid item sx={{ justifyContent: 'center' }}>
-                        <QuestionLogo width="90%" height="90%" />
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    p={2}
+                    sm
+                >
+                    <Grid item sx={{ textAlign: 'center', m: 3 }}>
+                        <Typography variant="h4" color="inherit">
+                            お困りですか?
+                        </Typography>
                     </Grid>
-                    <Grid item flex='auto' sx={{ mb: 2 }}>
-                        <Box component="form" onSubmit={handleSubmit}>
-                            <Box sx={{ mx: 1, my: 3 }}>
-                                <TextField
-                                    name="userName"
-                                    required
-                                    fullWidth
-                                    id="userName"
-                                    label="ユーザー名"
-                                    onClick={handleChange('userName')}
-                                    autoFocus
-                                />
+                    <Grid
+                        container
+                        justifyContent="space-evenly"
+                        alignItems="center"
+                        sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+                    >
+                        <Grid item>
+                            <QuestionLogo width="90%" height="90%" />
+                        </Grid>
+                        <Grid item sx={{ minWidth: "360px", mb: 2 }}>
+                            <Box component="form" onSubmit={handleSubmit}>
+                                <Box sx={{ mx: 1, my: 3 }}>
+                                    <TextField
+                                        name="userName"
+                                        required
+                                        fullWidth
+                                        id="userName"
+                                        label="ユーザー名"
+                                        onClick={handleChange('userName')}
+                                        autoFocus
+                                    />
+                                </Box>
+                                <Box sx={{ mx: 1, my: 3 }}>
+                                    <Email values={values} setValues={setValues} />
+                                </Box>
+                                <Box sx={{ mx: 1, my: 3 }}>
+                                    <TextareaAutosize
+                                        minRows={5}
+                                        style={{ width: '100%', borderRadius: 18, outline: 'none', fontSize: '1rem', padding: '1em' }}
+                                        onClick={handleChange('contents')}
+                                    />
+                                </Box>
+                                <Box sx={{ m: 2, textAlign: 'center' }}>
+                                    <Fab type="submit" variant="extended" color="primary">
+                                        <SendIcon />
+                                        送信する
+                                    </Fab>
+                                </Box>
                             </Box>
-                            <Box sx={{ mx: 1, my: 3 }}>
-                                <Email values={values} setValues={setValues} />
-                            </Box>
-                            <Box sx={{ mx: 1, my: 3 }}>
-                                <TextareaAutosize
-                                    minRows={5}
-                                    style={{ width: '100%', borderRadius: 18, outline: 'none', fontSize: '1rem', padding: '1em' }}
-                                    onClick={handleChange('contents')}
-                                />
-                            </Box>
-                            <Box sx={{ m: 2, textAlign: 'center' }}>
-                                <Fab type="submit" variant="extended" color="primary">
-                                    <SendIcon />
-                                    送信する
-                                </Fab>
-                            </Box>
-                        </Box>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
+
 
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                 <Footer />
