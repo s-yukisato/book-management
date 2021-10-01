32a16c234a5962d103b328bea7cc4225c5ba57a5
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-01 17:57:08 +0900

    clean code

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,72 +2,71 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
 
 import Logo from '../assets/logo_transparent.png';
 
 import Copyright from './Copyright'
 
 
+const CGrid = ({ children }) => (
+    <Grid
+        item
+        textAlign="center"
+        p={2}
+        xs={12}
+        sm={6}
+        md={3}
+    >
+        {children}
+    </Grid>
+)
+
+const listItems = [
+    {
+        title: "コンテンツ",
+        contents: ["ホーム", "ダッシュボード", "プロジェクト"]
+    },
+    {
+        title: "コンタクト",
+        contents: ["お問い合わせ", "ヘルプ"]
+    },
+    {
+        title: "管理者情報",
+        contents: ["Github", "...so on"]
+    },
+]
+
+
 const Footer = () => {
     return (
         <>
-            <Grid container sx={{ bgcolor: '#939597', p: 2 }}>
-                <Grid container xs={12} sm={3} sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-                    <img src={Logo} width="100%" height="100%" alt="Logo" />
-                </Grid>
-                <Grid container xs={12} sm={3} sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-                    <List>
-                        <ListItem>
-                            <Typography variant="h6">コンテンツ</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1"></Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1">ホーム</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1">プロジェクト</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1">ダッシュボード</Typography>
-                        </ListItem>
-                    </List>
-                </Grid>
-                <Grid container xs={12} sm={3} sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-                    <List>
-                        <ListItem>
-                            <Typography variant="h6">コンタクト</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1"></Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1">お問い合わせ</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1">ヘルプ</Typography>
-                        </ListItem>
-                    </List>
-                </Grid>
-                <Grid container xs={12} sm={3} sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-                    <List>
-                        <ListItem>
-                            <Typography variant="h6">最新情報</Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1"></Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1"></Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Typography variant="body1"></Typography>
-                        </ListItem>
-                    </List>
-                </Grid>
-                <Grid container sx={{ display: 'flex', justifyContent: 'center', m: 2 }}>
-                    <Copyright />
+            <Grid
+                container
+                sx={{ bgcolor: '#939597' }}
+            >
+                <CGrid>
+                    <img src={Logo} width="200px" height="200px" alt="Logo" />
+                </CGrid>
+
+                {listItems.map(listItem => (
+                    <CGrid>
+                        <List sx={{ display: 'inline-block' }}>
+                            <ListItem key={listItem.title}>
+                                <Typography variant="h6">
+                                    {listItem.title}
+                                </Typography>
+                            </ListItem>
+                            {listItem.contents.map(content => (
+                                <ListItemButton key={content}>
+                                    <Typography variant="body2">{content}</Typography>
+                                </ListItemButton>
+                            ))}
+                        </List>
+                    </CGrid>
+                ))}
+                <Grid item width="100vw">
+                    <Copyright m={2} />
                 </Grid>
             </Grid>
         </>
