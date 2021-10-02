06e406516c2d95038dabf82963d6592dd6b0bc11
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-02 16:24:10 +0900

    Update useFetch.js

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,18 +1,63 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import axios from "axios";
 
-export const useFetch = (url) => {
-  const [data, setData] = useState([]);
-  const [completed, setCompleted] = useState(false);
+export function useFetch(url) {
+  const cache = useRef({});
+
+  const cancelRequest = useRef(false);
+
+  const initialState = {
+    status: "idle",
+    error: null,
+    data: [],
+  };
+
+  const fetchReducer = (state, action) => {
+    switch (action.type) {
+      case "FETCHING":
+        return { ...initialState, status: "fetching" };
+      case "FETCHED":
+        return { ...initialState, status: "fetched", data: action.payload };
+      case "FETCH_ERROR":
+        return { ...initialState, status: "error", error: action.payload };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
 
   useEffect(() => {
+    if (!url) return;
+
+    console.log("fetching data");
+
     const fetchData = async () => {
-      await axios
-        .get(url)
-        .then((res) => setData(res.data));
-      setCompleted(true);
+      dispatch({ type: "FETCHING" });
+
+      if (cache.current[url]) {
+        const data = cache.current[url];
+        dispatch({ type: "FETCHED", payload: data });
+        return;
+      }
+      console.log("No catch");
+      try {
+        const { data } = await axios.get(url);
+        cache.current[url] = data;
+        // if (cancelRequest) return;
+        dispatch({ type: "FETCHED", payload: data });
+      } catch (error) {
+        // if (cancelRequest) return;
+        dispatch({ type: "FETCH_ERROR", payload: error.message });
+      }
     };
+
     fetchData();
+
+    return () => {
+      cancelRequest.current = true;
+    };
   }, [url]);
-  return [ data, completed ];
-};
+
+  return state;
+}
