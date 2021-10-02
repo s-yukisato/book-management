42c33d0ded4e9e12b668297fcd8be72adb594309
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-03 08:50:44 +0900

    update error handling

--- client/src/components/ContentsList.jsx
+++ client/src/components/ContentsList.jsx
@@ -5,32 +5,39 @@ import Card from './Card';
 
 import { useFetch } from '../hooks/useFetch';
 import { useFetchRecordContext } from '../context/FetchContext';
-import { useAuthContext } from '../context/AuthContext'
+import { useAuthContext } from '../context/AuthContext';
+
+const url = 'http://localhost:3001/api/data'
 
 const ContentList = () => {
-    const url = 'http://localhost:3001/api/data'
-    const [books, completed] = useFetch(url);
+    const { data: books, status } = useFetch(url);
+    console.log(books, status)
 
     const { user } = useAuthContext();
 
     const { dataState } = useFetchRecordContext();
     const records = dataState.data
 
-    const registeredList = records.length > 0 ? records.map((record) => record.book.isbn): [];
+    const registeredList = records.length > 0 ? records.map((record) => record.book.isbn) : [];
 
     return (
         <>
-            <Grid container spacing={2} sx={{ justifyContent: 'space-evenly', m: 'auto', width: "90vw" }}>
-                {completed ? (
-                    books.map((book, index) => (
-                        <Grid item key={index}>
+            <Grid
+                container
+                justifyContent='space-evenly'
+                spacing={2} sx={{ m: 'auto', width: "90vw" }}>
+                {status === "fetched" && (
+                    books.map(book => (
+                        <Grid item key={book.title}>
                             <Card book={book} registeredList={registeredList} user={user} />
                         </Grid>
-                    ))) : Array.from(new Array(30)).map(index => (
-                        <Grid item key={index} >
-                            <Skeleton />
-                        </Grid>
-                    ))}
+                    )))}
+                {(status === "fetching" || status === "idle") && Array.from(new Array(30)).map(index => (
+                    <Grid item key={index} >
+                        <Skeleton />
+                    </Grid>
+                ))}
+                {status === "error" && <>現在メンテナンス中です</>}
             </Grid>
         </>
     )
