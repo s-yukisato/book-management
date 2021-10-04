e045f7967f4c7524feb2f21e133a0ac774044191
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-04 10:52:19 +0900

    add redirect home and update style

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,12 +1,26 @@
+import { useHistory } from 'react-router-dom';
+
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 import { ReactComponent as NotFoundLogo } from '../assets/undraw_page_not_found_su7k.svg';
 
 const Error = () => {
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.replace("/")
+    }
     return (
         <>
-            <NotFoundLogo width="80%" height="80%"/>
-            <Typography variant="inherit">ページが見つかりません</Typography>
+            <Button onClick={handleClick} startIcon={<ArrowBackIcon />} sx={{ position: "fixed", top: "30px", left: "20px" }}>ホームページへ</Button>
+            <Box textAlign="center">
+                <NotFoundLogo width="80%" height="80%" />
+                <Typography variant="inherit" my={3}>ページが見つかりません</Typography>
+            </Box>
         </>
     )
 }
