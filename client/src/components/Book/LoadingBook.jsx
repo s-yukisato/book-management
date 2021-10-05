de253a850f03f50df30d291c65f6c658c1c23ab5
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-05 23:08:50 +0900

    Update LoadingBook.jsx

--- client/src/components/Book/LoadingBook.jsx
+++ client/src/components/Book/LoadingBook.jsx
@@ -1,11 +1,12 @@
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import Skeleton from '@mui/material/Skeleton';
 
 
 const LoadingBook = () => {
   return (
-    <>
+    <Grid item>
       <Card sx={{ width: 210, height: 280, mb: 1, p: 2 }}>
         <Skeleton variant="rectangular" width={180} height={130} />
         <Box sx={{ pt: 0.5 }}>
@@ -13,7 +14,7 @@ const LoadingBook = () => {
           <Skeleton width="70%" />
         </Box>
       </Card>
-    </>
+    </Grid>
   )
 };
 
