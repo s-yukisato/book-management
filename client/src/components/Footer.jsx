89bf4caf8f1cdf40bb65c7ecad58b2a256ebce29
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-06 13:28:32 +0900

    add mobile style nested list

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link as RouterLink } from 'react-router-dom';
 
 import Grid from '@mui/material/Grid';
@@ -5,6 +6,10 @@ import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 import Logo from '../assets/logo_transparent.png';
 
@@ -15,7 +20,6 @@ const CGrid = ({ children }) => (
     <Grid
         item
         textAlign="center"
-        p={2}
         xs={12}
         sm={6}
         md={3}
@@ -60,23 +64,31 @@ const listItems = [
         contents: [
             {
                 title: "Github",
-                link: "/"
+                url: "https://github.com/s-yukisato"
             },
             {
                 title: "Twitter",
-                link: "/"
+                url: "https://twitter.com/"
             },
         ]
     },
 ]
 
 
-const Footer = () => {
+const Footer = React.memo(() => {
+    const [open, setOpen] = useState(false);
+
+    console.log("rendereee")
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
     return (
         <>
             <Grid
                 container
-                sx={{ bgcolor: '#939597' }}
+                justifyContent="center"
+                sx={{ bgcolor: '#939597', p:2 }}
             >
                 <CGrid>
                     <img src={Logo} width="200px" height="200px" alt="Logo" />
@@ -84,26 +96,60 @@ const Footer = () => {
 
                 {listItems.map(listItem => (
                     <CGrid>
-                        <List sx={{ display: 'inline-block' }}>
+                        <List sx={{ display: { xs: "none", sm: 'inline-block'} }}>
                             <ListItem key={listItem.title}>
                                 <Typography variant="h6">
                                     {listItem.title}
                                 </Typography>
                             </ListItem>
-                            {listItem.contents.map(content => (
-                                <ListItemButton component={RouterLink} to={content.link} key={content.title}>
-                                    <Typography variant="body2">{content.title}</Typography>
-                                </ListItemButton>
+                            {listItem.contents.map(item => (
+                                item.link ? (
+                                    <ListItemButton component={RouterLink} to={item.link} key={item.title}>
+                                        <ListItemText primary={item.title} />
+                                    </ListItemButton>
+                                ) : (
+                                    <ListItemButton target="_blank" component={'a'} href={item.url} key={item.title}>
+                                        <ListItemText primary={item.title} />
+                                    </ListItemButton>
+                                )
                             ))}
                         </List>
                     </CGrid>
                 ))}
+
+                <List
+                    sx={{ width: '100%', maxWidth: 360, display: { xs: "block", sm: "none" } }}
+                    component="nav"
+                >
+                    {listItems.map(listItem => (
+                        <>
+                            <ListItemButton onClick={handleClick}>
+                                <ListItemText primary={listItem.title} />
+                                {open ? <ExpandLess /> : <ExpandMore />}
+                            </ListItemButton>
+
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                {listItem.contents.map(item => (
+                                    item.link ? (
+                                        <ListItemButton component={RouterLink} to={item.link} key={item.title}>
+                                            <ListItemText primary={item.title} />
+                                        </ListItemButton>
+                                    ) : (
+                                        <ListItemButton target="_blank" component={'a'} href={item.url} key={item.title}>
+                                            <ListItemText primary={item.title} />
+                                        </ListItemButton>
+                                    )
+                                ))}
+                            </Collapse>
+                        </>
+                    ))}
+                </List>
                 <Grid item width="100vw">
                     <Copyright m={2} />
                 </Grid>
             </Grid>
         </>
     )
-}
+});
 
 export default Footer;
