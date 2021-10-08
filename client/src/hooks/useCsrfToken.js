15680ee85907aaa8da4d8ed369c15a03519e649c
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-08 18:51:05 +0900

    Update useCsrfToken.js

--- client/src/hooks/useCsrfToken.js
+++ client/src/hooks/useCsrfToken.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import axios from "axios";
 
+import { API_URI } from "../config";
 
 export const useCsrfToken = () => {
   useEffect(() => {
-    const url = "http://localhost:3001";
     const getCsrfToken = async () => {
-      const { data } = await axios.get(`${url}/csrf-token`);
+      const { data } = await axios.get(`${API_URI}/csrf-token`);
       axios.defaults.headers.post["X-CSRF-Token"] = data.csrfToken;
     };
-    axios.defaults.withCredentials = true; 
+    axios.defaults.withCredentials = true;
     getCsrfToken();
   }, []);
 };
