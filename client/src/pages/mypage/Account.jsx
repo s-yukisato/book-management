7e62103fb369f80f9cc2b70043159e484585983e
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-08 17:46:25 +0900

    [Update] mypage move

---
+++ client/src/pages/mypage/Account.jsx
@@ -0,0 +1,117 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URI } from '../../config';
+
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import UserName from '../../components/FormParts/UserName';
+import Email from '../../components/FormParts/Email';
+import Password from '../../components/FormParts/Password';
+import Dialog from '../../components/block/Dialog';
+import Snackbar from '../../components/block/Snackbar';
+import Table from './UserTable';
+
+import { useAuthContext } from '../../context/AuthContext';
+import { useRedirect } from '../../hooks/useRedirect';
+
+
+const Account = () => {
+    const { user } = useAuthContext();
+    const { toHomePage } = useRedirect();
+
+    const [values, setValues] = useState({});
+    const [target, setTarget] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (!user) return;
+        setValues({
+            name: user.name,
+            email: user.email,
+            password: user.password
+        })
+    }, [user])
+
+    const [openDialog, setOpenDialog] = useState(false);
+    const closeDialog = () => setOpenDialog(false);
+
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+
+    const handleClick = (prop) => (e) => {
+        if (prop === 'name') {
+            setOpenSnackbar(true);
+        } else {
+            setOpenDialog(true);
+        }
+        setTarget(prop);
+    }
+
+    const update = async (e) => {
+        e.preventDefault();
+
+        setError(null);
+
+        await axios.post(`${API_URI}/api/v1/user`, values)
+            .then(res => res)
+            .catch(err => setError(err.message))
+
+        if (error) return setError("変更に失敗しました")
+
+        if (!error) {
+            setOpenDialog(false);
+            setOpenSnackbar(true);
+        }
+    }
+
+    const title = `${target}の変更`;
+
+    const content = (
+        <Box sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}><UserName values={values} setValues={setValues} /></Grid>
+                <Grid item xs={12}><Email values={values} setValues={setValues} /></Grid>
+                <Grid item xs={12}><Password values={values} setValues={setValues} /></Grid>
+            </Grid>
+            <Typography color="error" sx={{ textAlign: 'center', mt: 2 }}>{error}</Typography>
+        </Box>
+    )
+
+    const action = <Button onClick={update}>変更する</Button>
+
+    return (
+        <>
+            <Button
+                startIcon={<ArrowBackIcon />}
+                onClick={toHomePage}
+                sx={{ position: "fixed", top: "70px", left: "20px" }}
+            >
+                ホームページへ
+            </Button>
+            <Box
+                sx={{
+                    my: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main', width: '56px', height: '56px' }}>
+                    <AccountCircleIcon fontSize="large" />
+                </Avatar>
+                <Typography variant="h6">ようこそ {user.name} !</Typography>
+            </Box>
+            <Table user={user} handleClick={handleClick} />
+            <Dialog isOpen={openDialog} close={closeDialog} title={title} content={content} action={action} />
+            <Snackbar isOpen={openSnackbar} setIsOpen={setOpenSnackbar} message={`${target}を変更しました`} />
+        </>
+    )
+}
+
+export default Account;
---
+++ client/src/pages/mypage/UserTable.jsx
@@ -0,0 +1,88 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import EditIcon from '@mui/icons-material/Edit';
+import { Typography } from '@mui/material';
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: "#A0937D",
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: "rgba(255, 235, 201, 0.6)",
+    },
+    '&:nth-of-type(even)': {
+        backgroundColor: "rgba(249, 243, 223, 0.6)",
+    },
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+
+function createData(id, attributes, action) {
+    return { id, attributes, action };
+}
+
+const header = [
+    "属性",
+    "登録情報",
+    "ボタン"
+]
+
+const rows = [
+    createData('name', 'ユーザー名', '編集する'),
+    createData('email', 'メールアドレス', '編集する'),
+    createData('password', 'パスワード', '編集する'),
+];
+
+
+export default function Tables({ user, handleClick }) {
+
+
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ width: { xs: "90vw", sm: '80vw' } }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            {header.map(item => <StyledTableCell>{item}</StyledTableCell>)}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => (
+                            <StyledTableRow key={row.attributes}>
+                                <StyledTableCell>
+                                    {row.attributes}
+                                </StyledTableCell>
+                                <StyledTableCell align="left">
+                                    {user && user[row.id]}
+                                </StyledTableCell>
+                                <StyledTableCell align="left">
+                                    <Button variant="outlined" color="inherit" onClick={handleClick(row.id)} startIcon={<EditIcon />}>
+                                        <Typography sx={{ display: { xs: 'none', sm: 'block' } }}>{row.action}</Typography>
+                                    </Button>
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    );
+};
