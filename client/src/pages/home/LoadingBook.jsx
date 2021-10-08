338b3b8a791b0f853a688800a92388a70228c7c4
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-08 23:45:17 +0900

    [Update] use Memo

--- client/src/pages/home/LoadingBook.jsx
+++ client/src/pages/home/LoadingBook.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
+
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 import Skeleton from '@mui/material/Skeleton';
 
 
-const LoadingBook = () => {
+const LoadingBook = React.memo(() => {
   return (
     <Grid item>
-      <Card sx={{ width: 210, height: 280, mb: 1, p: 2, boxShadow: 2, bgcolor: "transparent" }}>
+      <Card sx={{ width: 210, height: 280, mb: 1, p: 2, boxShadow: 2, bgcolor: "transparent", cursor: "inherit" }}>
         <Skeleton variant="rectangular" width={180} height={130} />
         <Box sx={{ pt: 0.5 }}>
           <Skeleton />
@@ -16,6 +18,6 @@ const LoadingBook = () => {
       </Card>
     </Grid>
   )
-};
+});
 
 export default LoadingBook;
