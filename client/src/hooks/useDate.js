ef34a1beb1b9fb37bf5f553048c8a33761a53b24
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-08 23:47:56 +0900

    [delete] file

--- client/src/hooks/useDate.js
+++
@@ -1,6 +0,0 @@
-export const formatDate = (date) => {
-    var y = date.getFullYear();
-    var m = ('00' + (date.getMonth() + 1)).slice(-2);
-    var d = ('00' + date.getDate()).slice(-2);
-    return (y + '-' + m + '-' + d);
-};
