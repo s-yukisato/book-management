b4fa4d21d6940014f66c561783dda0b1a42be812
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-11 17:36:42 +0900

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from "axios";
+import { API_URI } from "./config";
 
 import Home from "./pages/home/Main";
 import Signin from "./pages/Signin";
@@ -26,10 +27,10 @@ import {
 axios.defaults.withCredentials = true;
 
 function App() {
+  
   useEffect(() => {
-    const url = "http://localhost:3001";
     const getCsrfToken = async () => {
-      const { data } = await axios.get(`${url}/csrf-token`);
+      const { data } = await axios.get(`${API_URI}/csrf-token`);
       axios.defaults.headers.post["X-CSRF-Token"] = data.csrfToken;
     };
     getCsrfToken();
@@ -40,7 +41,6 @@ function App() {
       <AuthProvider>
         <Router>
           <Switch>
-
             <Route exact path="/">
               <FetchRecordProvider>
                 <Home />
@@ -100,7 +100,6 @@ function App() {
             <Route path="*">
               <Error />
             </Route>
-            
           </Switch>
         </Router>
       </AuthProvider>
--- client/src/theme.js
+++ client/src/theme.js
@@ -2,9 +2,6 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
-        secondary: {
-            main: "#EE99A0"
-        }
     }
 });
 
