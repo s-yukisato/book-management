c2b33513a44043e9fdbdacae8460be90f9d1b937
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-12 13:45:00 +0900

    fix path

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URI = "http://localhost:3001"
+export const API_URI = "https://teelog.herokuapp.com"
--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://teelog.herokuapp.com/",
   })
 );
 
