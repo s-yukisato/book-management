a6eb732ad6a4947b64981ad2ecca81db25893db8
yuki <s1932058jl@s.chibakoudai.jp>
2021-10-15 11:05:34 +0900

    [Update] style

--- client/src/pages/createProject/Main.jsx
+++ client/src/pages/createProject/Main.jsx
@@ -5,7 +5,6 @@ import { API_URI } from '../../config';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Toolbar from '@mui/material/Toolbar';
 
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
@@ -58,8 +57,13 @@ const Main = () => {
                 startIcon={<ArrowBackIcon />}
                 sx={{ position: "fixed", top: "30px", left: "20px" }}
             >プロジェクト一覧へ戻る</Button>
-            <Toolbar />
-            <Box sx={{ textAlign: "center", mx: { xs: 2, sm: 14 }, my: 4 }}>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: "100vh"
+            }}>
                 <Stepper values={values} setValues={setValues} create={createProject} />
             </Box>
         </>
